8edaae6fe5244be55d72769389458eedf85334f6
shahargalon <shahargalon@gmail.com>
2021-07-16 18:44:16 +0300

    create main trf file with ec2 instance

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+    region = "eu-central-1"
+    ***REMOVED***  # need to change this 
+    ***REMOVED***  # need to change this
+}
+
+resource "aws_instance" "cassandra" {
+    ami = "ami-0b1deee75235aa4bb"
+    instance_type = "t2.micro"
+   # availability_zone = "eu-central-1"
+    #associate_public_ip_address = ??? maybe
+    security_groups = ["allow_ssh"]
+    count = 2
+    key_name = "access-key.ec2"
+    tags = {
+    Name = element(var.machine_number, count.index)
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+ 
+
+  ingress {
+    description      = "SSH traffic"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+    
+  }
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "machine_number" {
+  type        = list
+  default     = ["1","2"]
+
+}
